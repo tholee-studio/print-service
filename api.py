438119f960c904a3987135d180743f8ddc4276c8
Firman Adiyuda <firmanadiyuda@gmail.com>
2025-09-24 01:42:30 +0700

    feat: enable CORS support for the Flask application

--- api.py
+++ api.py
@@ -2,9 +2,12 @@ from bridge import bridge
 from printer import printer
 from thermal import thermal
 from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
 from waitress import serve
 
 app = Flask(__name__)
+
+CORS(app)
 
 
 @app.route("/print", methods=["POST"])
